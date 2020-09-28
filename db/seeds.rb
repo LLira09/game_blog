cf3851a8d3b2c1c8b05f49fa806800345ee64c08
github username <github email address>
2020-09-28 17:30:40 -0500

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Walid", email: "walid@walid.com", password: "12345678")
+User.create(username: "jake1", email: "jake1@jake1.com", password: "12345678")
+
+Category.create(name: "FPS")
+Category.create(name: "MMO")
+Category.create(name: "MOBA")
+Category.create(name: "PC")
+Category.create(name: "Xbox")
+Category.create(name: "Playstation")
+Category.create(name: "Nintendo")
+
+Article.create(title: "Article 1", content: "ucas ipsum dolor sit amet darth skywalker mara ben organa vader", user_id: 1)
+Article.create(title: "Article 2", content: "ucas ipsum dolor sit amet darth skywalker mara ben organa vader", user_id: 2)
+Article.create(title: "Article 3", content: "ucas ipsum dolor sit amet darth skywalker mara ben organa vader", user_id: 3)
+
+ArticleCategory.create(article_id: 1, category_id: 1)
+ArticleCategory.create(article_id: 2, category_id: 3)
+ArticleCategory.create(article_id: 3, category_id: 2)
