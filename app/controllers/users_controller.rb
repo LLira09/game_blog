148f0532b23b0d77c43e712c181f1cc901c3e4a8
github username <github email address>
2020-09-29 13:42:25 -0500

    added logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+
+    def index 
+        @users = User.all
+    end 
+
     def new
         @user = User.new
     end
@@ -13,6 +18,8 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
+            flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
             redirect_to @user
         else
             render 'new'
@@ -21,6 +28,7 @@ class UsersController < ApplicationController
 
     def update
        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
             redirect_to @user 
        else
             render 'edit'
