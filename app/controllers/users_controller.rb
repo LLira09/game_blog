00c4523e42939d4deb3019010d62472cdffc73ea
github username <github email address>
2020-09-29 10:00:06 -0500

    Added edit user to the users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-
+    before_action :set_user, only: [:show, :edit, :update]
     def new
         @user = User.new
     end
 
     def show
-        @user = User.find(params[:id])
+    end
+
+    def edit
     end
 
     def create
@@ -17,9 +19,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+       if @user.update(user_params)
+            redirect_to @user 
+       else
+            render 'edit'
+       end
+    end
+
     private 
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 end
