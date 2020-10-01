4b9026574fe3f23d961957b5a4b6c0ca06edafa6
wyounis25 <github email address>
2020-10-01 09:48:58 -0500

    validation check

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,24 @@ class CommentsController < ApplicationController
 
 
 def create 
+    byebug
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.new(params[:comment].permit(:comment))
-    @comment.user = current_user
+    @comment = @article.comments.new(params[:comment].permit(:comment, :current_user, :article_id))
     @comment.save
     redirect_to article_path(@article)  
+
+    if !@comment.valid?
+        flash[:errors] = @comment.errors.full_messages
+      else 
+        @comment.save
+        redirect_to article_path(@comment)
+      end
+      
 end
+
+# def comment_params
+#     params.require(:comment).permit(:comment, :current_user, :article_id)
+# end 
+
 
 end
